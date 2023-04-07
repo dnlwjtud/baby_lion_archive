058e9079d57f3ede7b39c4fbee9f8b9617020a28
dnlwjtud <archaco124@gmail.com>
2023-04-07 22:12:13 +0900

    230407 Part3. get-post 매핑

--- myBlog/src/main/java/io/dnlwjtud/myBlog/posts/controller/PostController.java
+++ myBlog/src/main/java/io/dnlwjtud/myBlog/posts/controller/PostController.java
@@ -2,9 +2,7 @@ package io.dnlwjtud.myBlog.posts.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PostController {
@@ -13,5 +11,19 @@ public class PostController {
     public String getPostWriteView() {
         return "/posts/post_write";
     }
+
+    @PostMapping("/posts/write")
+    public String postWrite(
+            String title, String body
+    ) {
+        System.out.println("title = " + title);
+        System.out.println("body = " + body);
+        return "redirect:/";
+    }
+    //
+    // HTTP Method
+    // GET, POST
+
+
 
 }
