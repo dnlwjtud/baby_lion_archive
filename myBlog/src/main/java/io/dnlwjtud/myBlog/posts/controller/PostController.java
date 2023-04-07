366a56aa7898544698cb45ac6abeb4cdaaeaef25
dnlwjtud <archaco124@gmail.com>
2023-04-07 21:07:53 +0900

    230407 Part2. html 파일 추가

--- myBlog/src/main/java/io/dnlwjtud/myBlog/posts/controller/PostController.java
+++ myBlog/src/main/java/io/dnlwjtud/myBlog/posts/controller/PostController.java
@@ -3,15 +3,15 @@ package io.dnlwjtud.myBlog.posts.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class PostController {
 
-    @GetMapping("/test")
-    public String test(Model model) {
-        return "test";
+    @GetMapping("/posts/write")
+    public String getPostWriteView() {
+        return "/posts/post_write";
     }
-
 
 }
