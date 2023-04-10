6e4814e6bf527966ce84ee295cef14cc6d219dcc
우철승 <eddie@ucheolseung-ui-MacBookPro.local>
2023-04-10 20:27:24 +0900

    230410 Part1. 포스트 작성기능

--- myBlog/src/main/java/io/dnlwjtud/myBlog/posts/controller/PostController.java
+++ myBlog/src/main/java/io/dnlwjtud/myBlog/posts/controller/PostController.java
@@ -1,11 +1,16 @@
 package io.dnlwjtud.myBlog.posts.controller;
 
+import io.dnlwjtud.myBlog.posts.service.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequiredArgsConstructor
 public class PostController {
+
+    private final PostService postService;
 
     @GetMapping("/posts/write")
     public String getPostWriteView() {
@@ -18,10 +23,18 @@ public class PostController {
     ) {
         System.out.println("title = " + title);
         System.out.println("body = " + body);
+
+        Long savedId = postService.save(title, body);
+
+        System.out.println("savedId = " + savedId);
+
         return "redirect:/";
     }
-    //
-    // HTTP Method
+
+    // Spring MVC
+    // Spring 요청 - 응답 흐름
+    // Request -> Controller -> Service -> Repository -> Service -> Controller -> Response
+    // HTTP Method¡
     // GET, POST
 
 
