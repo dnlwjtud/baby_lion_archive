38de4863776ae0e33e9d9390a6c4d2e1647267c6
dnlwjtud <archaco124@gmail.com>
2023-04-20 19:16:57 +0900

    230419 Part1. Security RequestMatchers 설정

--- myBlog/src/main/java/io/dnlwjtud/myBlog/global/config/SecurityConfig.java
+++ myBlog/src/main/java/io/dnlwjtud/myBlog/global/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.stereotype.Controller;
 
 @Configuration
@@ -14,8 +15,19 @@ import org.springframework.stereotype.Controller;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        return http.build();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf()
+            .and()
+                .authorizeHttpRequests()
+                    .requestMatchers("/posts/**")
+                        .permitAll() // FIXME: Role 구현 후 수정
+                    .requestMatchers("/admin/**")
+                        .permitAll() // FIXME: Role 구현 후 수정
+                    .anyRequest()
+                        .permitAll()
+            .and()
+                .build();
     }
 
 }
